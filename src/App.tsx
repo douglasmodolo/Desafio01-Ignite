8699a4b2ae1362461236d9f6b490d1ecee25f0af
Douglas <douglasmodolo@outlook.com.br>
2025-02-18 20:54:32 -0300

    Finalização toDoList

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,80 @@ export function App() {
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
-
+    
     if (newTask.trim() === '') return; 
 
-    const newTaskObject: TaskType = {
-      id: tasks.length + 1,
-      tittle: newTask,
-      isCompleted: false,
-    }
+    // const newTaskObject: TaskType = {
+    //   id: tasks.length + 1,
+    //   title: newTask,
+    //   isChecked: false,
+    // }
 
-    setTasks([...tasks, newTaskObject]);
-    setNewTask('');
+    // setTasks([...tasks, newTaskObject]);
+    // setNewTask('');
+
+    // handleUpdateCompletedInfo(tasks);
+
+    setTasks((prevTasks) => {
+      const newTaskObject: TaskType = {
+        id: prevTasks.length + 1,
+        title: newTask,
+        isChecked: false,
+      };
+  
+      const updatedTasks = [...prevTasks, newTaskObject];
+  
+      handleUpdateCompletedInfo(updatedTasks); // Chama com a lista atualizada
+      return updatedTasks;
+    });
+  
+    setNewTask('');    
   }
 
   function handleCountTask() {
-    setTaskCount(taskCount + 1);
+    const newTaskCount = taskCount + 1;
+    setTaskCount(newTaskCount);
+    handleUpdateCompletedInfo(tasks);
   }
+
+  // function handleUpdateCompletedInfo(taskCount: number) {
+  //   const totalCompletedTasks = tasks.filter(task => task.isChecked).length;
+  //   setCompletedTaskCount(`${totalCompletedTasks} de ${taskCount}`);
+  // }
+
+  function handleUpdateCompletedInfo(tasksList: TaskType[]) {
+    const totalCompletedTasks = tasksList.filter(task => task.isChecked).length;
+    setCompletedTaskCount(`${totalCompletedTasks} de ${tasksList.length}`);
+  }
+
+  function deleteTask(taskToDelete: TaskType) {
+    const tasksWithoutDeletedOne = tasks.filter(task => {
+      return task.id !== taskToDelete.id;
+    });
+
+    setTasks(tasksWithoutDeletedOne);
+    const newTaskCount = taskCount - 1;
+    setTaskCount(newTaskCount);
+    handleUpdateCompletedInfo(tasksWithoutDeletedOne);
+  }
+
+  function handleToggleTask({ id, value }: { id: number, value: boolean }) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isChecked: value };
+      }
+  
+      return task;
+    });
+  
+    setTasks(updatedTasks); 
+    handleUpdateCompletedInfo(updatedTasks);
+
+    // const totalCompletedTasks = updatedTasks.filter(task => task.isChecked).length;
+    // setCompletedTaskCount(`${totalCompletedTasks} de ${taskCount}`);
+  }
+
+  const isNewTaskEmpty = newTask.length == 0;
 
   return (
     <>
@@ -44,7 +102,7 @@ export function App() {
           onChange={(e) => setNewTask(e.target.value)}
         />
 
-        <button type='submit' onClick={handleCountTask}>
+        <button type='submit' disabled={isNewTaskEmpty} onClick={handleCountTask}>
           Criar
         </button>
       </form>
@@ -52,17 +110,24 @@ export function App() {
       <main className={styles.main}>        
         <div className={styles.wrapper}>
           <div className={styles.tasksHeader}>
-            <span>
-              Tarefas criadas <span className={styles.count}>{taskCount}</span>
-            </span>
-            <span>
-              Concluídas <span className={styles.count}>{completedTaskCount}</span>
-            </span>
+            <div className={styles.createdTasks}>
+              <span>Tarefas criadas </span>
+              <span className={styles.count}>{taskCount}</span>              
+            </div>
+            <div className={styles.completedTasks}>
+              <span>Concluídas </span>
+              <span className={styles.count}>{completedTaskCount}</span>
+            </div>
           </div>
 
           {tasks.map(task => {
             return (
-              <Task key={task.id} task={task}/>
+              <Task 
+                key={task.id} 
+                task={task}
+                onDeleteTask={deleteTask}
+                toggleTaskStatus={handleToggleTask}
+              />
             )
           })}
         </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,21 +1,49 @@
+import { Check, Trash } from '@phosphor-icons/react';
+
 import styles from './Task.module.css';
 
 export interface TaskType {
     id: number;
-    tittle: string;
-    isCompleted: false;
+    title: string;
+    isChecked: boolean;
 }
 
 interface TaskProps{
-    task: TaskType
+    task: TaskType;
+    onDeleteTask: (task: TaskType) => void;
+    toggleTaskStatus: ({id, value}: { id: number; value: boolean}) => void;
 }
 
-export function Task({ task }: TaskProps) {
+export function Task({ task, onDeleteTask, toggleTaskStatus }: TaskProps) {
+    const checkboxCheckedClassname = task.isChecked
+    ? styles['checkbox-checked']
+    : styles['checkbox-unchecked']
+
+    function handleTaskToggle() {
+        toggleTaskStatus({ id: task.id, value: !task.isChecked })
+    }
+
+    function handleDeleteTask() {
+        onDeleteTask(task);
+    }
+
     return (
         <div className={styles.task}>
-            <input type="checkbox" checked={task.isCompleted} readOnly/>
-            <span>{task.tittle}</span>
-            <button>Excluir</button>
+            {/* <button onClick={handleCompleteTask} title='Completar task' className={styles.buttonCompleteTask}>
+                <RadioButton size={24}/>
+            </button> */}
+            
+            <label htmlFor="checkbox" onClick={handleTaskToggle}>
+                <input readOnly type="checkbox" checked={task.isChecked} />
+                <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
+                    {task.isChecked && <Check size={12} />}
+                </span>
+            </label>
+            
+            <span>{task.title}</span>
+            <button onClick={handleDeleteTask} title='Deletar task' className={styles.buttonDeleteTask}>
+                <Trash size={20} />
+            </button>
         </div>
     );
 }
